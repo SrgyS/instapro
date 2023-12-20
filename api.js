481ebaa011082aec0a0a9e02c953cbfeaeceba19
SrgyS <stepanov_sr@mail.ru>
2023-12-20 18:44:34 +0300

    Update readme file

--- api.js
+++ api.js
@@ -1,153 +1,146 @@
-// Замени на свой, чтобы получить независимый от других набор данных.
-// "боевая" версия инстапро лежит в ключе prod
-const personalKey = "sergei-stepanov";
-const baseHost = "https://webdev-hw-api.vercel.app";
+const personalKey = 'sergei-stepanov';
+const baseHost = 'https://webdev-hw-api.vercel.app';
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
 
 export function getPosts({ token }) {
-  return fetch(postsHost, {
-    method: "GET",
-    headers: {
-      Authorization: token,
-    },
-  })
-    .then((response) => {
-      if (response.status === 401) {
-        throw new Error("Нет авторизации");
-      }
-
-      return response.json();
+    return fetch(postsHost, {
+        method: 'GET',
+        headers: {
+            Authorization: token,
+        },
     })
-    .then((data) => {
-      return data.posts;
+        .then((response) => {
+            if (response.status === 401) {
+                throw new Error('Нет авторизации');
+            }
+
+            return response.json();
+        })
+        .then((data) => {
+            return data.posts;
+        });
+}
+
+export function registerUser({ login, password, name, imageUrl }) {
+    return fetch(baseHost + '/api/user', {
+        method: 'POST',
+        body: JSON.stringify({
+            login,
+            password,
+            name,
+            imageUrl,
+        }),
+    }).then((response) => {
+        if (response.status === 400) {
+            throw new Error('Этот login занят');
+        }
+        return response.json();
     });
 }
 
-// https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
-export function registerUser({ login, password, name, imageUrl }) {
-  return fetch(baseHost + "/api/user", {
-    method: "POST",
-    body: JSON.stringify({
-      login,
-      password,
-      name,
-      imageUrl,
-    }),
-  }).then((response) => {
-    if (response.status === 400) {
-      throw new Error("Этот login занят");
-    }
-    return response.json();
-  });
-}
-
 export function loginUser({ login, password }) {
-  return fetch(baseHost + "/api/user/login", {
-    method: "POST",
-    body: JSON.stringify({
-      login,
-      password,
-    }),
-  }).then((response) => {
-    if (response.status === 400) {
-      throw new Error("Неверный логин или пароль");
-    }
-    return response.json();
-  });
+    return fetch(baseHost + '/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            login,
+            password,
+        }),
+    }).then((response) => {
+        if (response.status === 400) {
+            throw new Error('Неверный логин или пароль');
+        }
+        return response.json();
+    });
 }
 
 // Загружает картинку в облако, возвращает url загруженной картинки
 export function uploadImage({ file }) {
-  const data = new FormData();
-  data.append("file", file);
+    const data = new FormData();
+    data.append('file', file);
 
-  return fetch(baseHost + "/api/upload/image", {
-    method: "POST",
-    body: data,
-  }).then((response) => {
-    return response.json();
-  });
+    return fetch(baseHost + '/api/upload/image', {
+        method: 'POST',
+        body: data,
+    }).then((response) => {
+        return response.json();
+    });
 }
 
 // Получает посты конкретного пользователя
 export function getUserPosts({ token, id }) {
-  return fetch(postsHost +"/user-posts/" + id, {
-    method: "GET",
-    headers: {
-      Authorization: token,
-    },
-  })
-    .then((response) => {
-      if (response.status === 401) {
-        throw new Error("Нет авторизации");
-      }
-
-      return response.json();
+    return fetch(postsHost + '/user-posts/' + id, {
+        method: 'GET',
+        headers: {
+            Authorization: token,
+        },
     })
-    .then((data) => {
-      return data.posts;
-    });
+        .then((response) => {
+            if (response.status === 401) {
+                throw new Error('Нет авторизации');
+            }
+
+            return response.json();
+        })
+        .then((data) => {
+            return data.posts;
+        });
 }
 // Добавляет пост
 export function addPost({ description, imageUrl, token }) {
-  return fetch(postsHost, {
-    method: "POST",
-    headers: {
-      Authorization: token,
-    },
-    body: JSON.stringify({
-     description,
-      imageUrl,
-    }),
-  }
-  ).then((response) => {
-    if (response.status === 400) {
-      throw new Error("Отсутствует фото или описание");
-    }
-    if (response.status === 401) {
-        throw new Error("Нет авторизации");
-      }
-    return response.json();
-  });
+    return fetch(postsHost, {
+        method: 'POST',
+        headers: {
+            Authorization: token,
+        },
+        body: JSON.stringify({
+            description,
+            imageUrl,
+        }),
+    }).then((response) => {
+        if (response.status === 400) {
+            throw new Error('Отсутствует фото или описание');
+        }
+        if (response.status === 401) {
+            throw new Error('Нет авторизации');
+        }
+        return response.json();
+    });
 }
 
 //  Добавляет лайк
 
-export function addLike({token, id}) {
-  return fetch(postsHost + "/" + id + "/like", {
-    method: "POST",
-    headers: {
-      Authorization: token,
-    },
-   }) 
-  .then((response) => {
-    return response.json();
-  });
+export function addLike({ token, id }) {
+    return fetch(postsHost + '/' + id + '/like', {
+        method: 'POST',
+        headers: {
+            Authorization: token,
+        },
+    }).then((response) => {
+        return response.json();
+    });
 }
 
 //  Удаляет лайк
 
-export function removeLike({token, id}) {
-  return fetch(postsHost + "/" + id + "/dislike", {
-    method: "POST",
-    headers: {
-      Authorization: token,
-    },
-   }) 
-  .then((response) => {
-    return response.json();
-  });
+export function removeLike({ token, id }) {
+    return fetch(postsHost + '/' + id + '/dislike', {
+        method: 'POST',
+        headers: {
+            Authorization: token,
+        },
+    }).then((response) => {
+        return response.json();
+    });
 }
 //  Удаляет пост
 
-export function deletePost({token, id}) {
-  return fetch(postsHost + "/" + id, {
-    method: "DELETE",
-    headers: {
-      Authorization: token,
-    },
-   }) 
-  .then((response) => {
-    return response.json();
-  });
+export function deletePost({ token, id }) {
+    return fetch(postsHost + '/' + id, {
+        method: 'DELETE',
+        headers: {
+            Authorization: token,
+        },
+    }).then((response) => {
+        return response.json();
+    });
 }
